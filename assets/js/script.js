99d0bd34a805b6fe3f28e6b38314eb517a7ff9e6
Mery <meryem.achemlal@alterway.fr>
2020-11-30 18:24:47 +0100

    Retours datalab (WIP)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,56 @@
 console.log("TEST");
-var fn_scrollup = function () {
-    if ($( "#scrollTop" ).length) {
-      $("#scrollTop").on('click', function() {
-        $("html, body").animate(
-          {scrollTop: "0"}, 2000);
-      });
-    }
- }
+// var fn_scrollup = function () {
+//     if ($( "#scrollTop" ).length) {
+//       $("#scrollTop").on('click', function() {
+//         $("html, body").animate(
+//           {scrollTop: "0"}, 2000);
+//       });
+//     }
+//  }
 
-var fn_scrolldown = function () {
-    $("#scrollDown").on('click', function() {
-        var n = $(document).height();
-        $('html, body').animate(
-            { scrollTop: n }, 500);
-    });
-}
+// var fn_scrolldown = function () {
+//     $("#scrollDown").on('click', function() {
+//         var n = $(document).height();
+//         $('html, body').animate(
+//             { scrollTop: n }, 500);
+//     });
+// }
 
 
-$( document ).ready(function() {
-    fn_scrollup();
-    fn_scrolldown();
-});
+// $( document ).ready(function() {
+//     fn_scrollup();
+//     fn_scrolldown();
+// });
+
+
+
+
+
+
+
+
+
+
+
+// Van js
+
+//  (function () {
+//       var el_top = document.querySelector("#scrollTop");
+//       if(el_top){
+//         el_top.addEventListener('click', function() {
+//         document.querySelector("html, body").animate(
+//           {scrollTop: "0"}, 2000);
+//       });
+//     }
+//  })();
+
+//  (function () {
+//   var el_down = document.querySelector("#scrollDown");
+//   if(el_down){
+//     el_down.addEventListener('click', function() {
+//         var n = document.querySelector(document).height();
+//         document.querySelector('html, body').animate(
+//             { scrollTop: n }, 500);
+//     });
+//   }
+// })();
